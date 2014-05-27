18503a99f621ea42e96b2e288fce8f599162e4d6
Becca Aaronson <becca.aaronson@gmail.com>
2014-05-27 17:22:06 -0500

    mobile first scss; toggleClass header
    redesigned _theme and _layout for mobile first; changed sticky header
    to .toggleClass;

--- app/scripts/script.js
+++ app/scripts/script.js
@@ -3,19 +3,13 @@ $(document).ready(function() {
 });
 
 
-  $(function(){
-        var stickyRibbonTop = $('#nav-header').offset().top;
-          
-        $(window).scroll(function(){
-                if( $(window).scrollTop() > stickyRibbonTop) {
-                        $('#nav-header').css({position: 'fixed', top: '0px', background: 'rgba(146,154,163,1)', width: $(".story-container").css('width')});
-                } else {
-                        $('#nav-header').css({position: 'static', top: '0px', background: 'rgba(146,154,163,0.5)'});
-                }
-        });
+$(function(){
+	var $window = $(window),
+		$stickyEl = $('#nav-header'),
+		elTop = $stickyEl.offset().top;
+	$window.scroll(function() {
+		$stickyEl.toggleClass('sticky', $window.scrollTop() > elTop);
+	});
 
-		$(window).resize(function(){
-				$('#nav-header').css({width: $(".story-container").css('width'), height: $(".lead-image".css('height'))});
-							});
 });
 
